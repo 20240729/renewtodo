8292134a02af52fb9694ef361dca7852c0a058dd
20240729 <yoohyeonsik@gmail.com>
2024-08-30 14:07:58 +0900

    init commit

---
+++ src/main/java/com/sparta/renewtodo/RenewtodoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.renewtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenewtodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenewtodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/renewtodo/RenewtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.renewtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenewtodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
